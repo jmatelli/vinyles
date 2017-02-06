f52615705c79ddcb06c123512e15de744fbb0e67
Joël Matelli <joel.matelli@tellijo.fr>
2017-02-06 09:33:35 +0100

    fix collection page

--- vuejs/src/pages/Vinyles.vue
+++ vuejs/src/pages/Vinyles.vue
@@ -1,10 +1,8 @@
 <template>
   <page-content page-title="Vinyles - Collection">
     <div class="main-content">
-        <h2>Your Vinyl Records Collection</h2>
-      <div v-if="vinyles.length">
-        <album-list :initial-list="vinyles"></album-list>
-      </div>
+      <h2>Your Vinyl Records Collection</h2>
+      <album-list v-if="vinyles.length" v-bind:list="vinyles"></album-list>
       <div v-else>
         <p>No vinyl record in your collection yet, you can search for one by clicking here</p>
         <md-button class="md-raised md-primary" v-on:click="openSearch()">Search for records</md-button>
